c6f6a77114deccbb419132792c8b1f13a673bd7e
Michal Miciak <maq0297@gmail.com>
2018-05-17 23:26:59 +0200

    adding JavaScript to set active class to current month

---
+++ MyFinance/wwwroot/js/month.js
@@ -0,0 +1,7 @@
+﻿{
+    var lis = $('.month').removeClass('active');
+    var url = location.href;
+    var month = url.split('=');
+    var newActive = $('#li' + month[1]);
+    newActive.addClass('active');
+}
